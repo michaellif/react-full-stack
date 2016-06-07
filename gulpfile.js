4b7fa0afd2d279d57fb4abdbcea3659f10f8a65d
Michael Lifshits <michael.lifshits@capitalone.com>
2016-06-07 14:25:43 -0400

    First implementation of router

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,43 @@ var gutil = require('gulp-util');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var connect = require('gulp-connect');
 
-gulp.task('build', function() {
-	return browserify({
+gulp.task('bundle', function() {
+	browserify({
 		entries : './app/js/app.js',
 		debug : true
-	})
-	.transform("babelify", {presets: ["react"]})
-	.bundle()
-	.pipe(source('bundle.js'))
-	.pipe(gulp.dest('dist'));
+	}).transform("babelify", {
+		presets : [ "es2015", "react" ]
+	}).bundle().pipe(source('bundle.js')).pipe(gulp.dest('dist'));
+	console.log('bundle.js has been rebuilt');
 });
 
-gulp.task('watch', [ 'build' ], function() {
-	gulp.watch('app/**/*.js', [ 'build' ]);
+gulp.task('nonjs', function() {
+	gulp.src('app/**/*.!(js)').pipe(gulp.dest('dist'));
+	console.log('nonjs files have been copied');
 });
 
-gulp.task('default', [ 'watch' ]);
+gulp.task('watch', [ 'bundle', 'nonjs' ], function() {
+	gulp.watch('app/js/**/*.js', [ 'bundle' ]);
+	gulp.watch('app/**/*.!(js)', [ 'nonjs' ]);
+});
+
+gulp.task('connect', function() {
+	connect.server();
+});
+
+gulp.task('connect', function() {
+	connect.server({
+		root : 'dist',
+		livereload : true,
+		port : 8282,
+		host : 'localhost'
+	});
+});
+
+gulp.task('clean', function(callback) {
+	rimraf('./dist', callback);
+});
+
+gulp.task('default', [ 'watch', 'connect' ]);
