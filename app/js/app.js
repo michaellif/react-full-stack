328621381401cafea80f9da97a185d295a5e660a
Michael Lifshits <michael.lifshits@capitalone.com>
2016-06-07 15:30:56 -0400

    Switched to IntelliJ

--- app/js/app.js
+++ app/js/app.js
@@ -1,85 +1,85 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { browserHistory, Router, Route, Link, withRouter } from 'react-router'
+import {browserHistory, Router, Route, Link, withRouter} from 'react-router'
 
 const App = React.createClass({
-	  render() {
-	    return (
-	      <div>
-	        <ul>
-	          <li><Link to="/dashboard" activeClassName="active">Dashboard</Link></li>
-	          <li><Link to="/form" activeClassName="active">Form</Link></li>
-	        </ul>
-	        {this.props.children}
-	      </div>
-	    )
-	  }
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li><Link to="/dashboard" activeClassName="active">Dashboard</Link></li>
+                    <li><Link to="/form" activeClassName="active">Form</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
 })
 
 const Dashboard = React.createClass({
-	  render() {
-	    return <h1>Dashboard</h1>
-	  }
+    render() {
+        return <h1>Dashboard</h1>
+    }
 })
 
 const Form = withRouter(
-	  React.createClass({
+    React.createClass({
 
-	    componentWillMount() {
-	      this.props.router.setRouteLeaveHook(
-	        this.props.route,
-	        this.routerWillLeave
-	      )
-	    },
+        componentWillMount() {
+            this.props.router.setRouteLeaveHook(
+                this.props.route,
+                this.routerWillLeave
+            )
+        },
 
-	    getInitialState() {
-	      return {
-	        textValue: 'ohai'
-	      }
-	    },
+        getInitialState() {
+            return {
+                textValue: 'ohai'
+            }
+        },
 
-	    routerWillLeave() {
-	      if (this.state.textValue)
-	        return 'You have unsaved information, are you sure you want to leave this page?'
-	    },
+        routerWillLeave() {
+            if (this.state.textValue)
+                return 'You have unsaved information, are you sure you want to leave this page?'
+        },
 
-	    handleChange(event) {
-	      this.setState({
-	        textValue: event.target.value
-	      })
-	    },
+        handleChange(event) {
+            this.setState({
+                textValue: event.target.value
+            })
+        },
 
-	    handleSubmit(event) {
-	      event.preventDefault()
+        handleSubmit(event) {
+            event.preventDefault()
 
-	      this.setState({
-	        textValue: ''
-	      }, () => {
-	        this.props.router.push('/')
-	      })
-	    },
+            this.setState({
+                textValue: ''
+            }, () => {
+                this.props.router.push('/')
+            })
+        },
 
-	    render() {
-	      return (
-	        <div>
-	          <form onSubmit={this.handleSubmit}>
-	            <p>Click the dashboard link with text in the input.</p>
-	            <input type="text" ref="userInput" value={this.state.textValue} onChange={this.handleChange} />
-	            <button type="submit">Go</button>
-	          </form>
-	        </div>
-	      )
-	    }
-	  })
+        render() {
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <p>Click the dashboard link with text in the input.</p>
+                        <input type="text" ref="userInput" value={this.state.textValue} onChange={this.handleChange}/>
+                        <button type="submit">Go</button>
+                    </form>
+                </div>
+            )
+        }
+    })
 )
 
 ReactDOM.render((
-	  <Router history={browserHistory}>
-	    <Route path="/" component={App}>
-	      <Route path="dashboard" component={Dashboard} />
-	      <Route path="form" component={Form} />
-	    </Route>
-	  </Router>
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="dashboard" component={Dashboard}/>
+            <Route path="form" component={Form}/>
+        </Route>
+    </Router>
 ), document.getElementById('root'))
 	
 	
